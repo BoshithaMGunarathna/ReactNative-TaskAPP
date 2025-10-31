71616111fbeec1e18d4fb727e6de28e5c78164c2
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-10-31 12:37:50 +0530

    Update App.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -1,24 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './src/store/store';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './src/screens/LoginScreen';
+import MainScreen from './src/screens/MainScreen';
+
+export type RootStackParamList = {
+  Login: undefined;
+  Main: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen 
+            name="Login" 
+            component={LoginScreen}
+            options={{ title: 'Login' }}
+          />
+          <Stack.Screen 
+            name="Main" 
+            component={MainScreen}
+            options={{ title: 'Messages' }}
+          />
+        </Stack.Navigator>
         <StatusBar style="auto" />
-      </View>
+      </NavigationContainer>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
